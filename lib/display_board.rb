4ee8c6813e137371dfdacbefde54f0e3f85cef50
ADIL NAIMI <adilnm23@hotmail.com>
2019-01-26 08:03:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,10 @@
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board()
-    puts "#{board[0]}| #{board[0]}  | #{board[0]}  "
+    puts "#{board[0]}| #{board[0]}| #{board[0]}"
     puts "-----------"
-    puts "  #{board[0]} |#{board[0]}   | #{board[0]}  "
+    puts "#{board[0]}| #{board[0]}| #{board[0]}"
     puts "-----------"
-    puts " #{board[0]}  |#{board[0]}   |#{board[0]}   "
+    puts "#{board[0]}| #{board[0]}| #{board[0]}"
 end
 display_board()
